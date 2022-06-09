9bae3232feea6d14e045a7f2c54c220b45318a29
antdev1987 <antdev1987@gmail.com>
2022-06-08 20:22:08 -0600

    api de login terminada

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,37 @@
 import generarJWT from "../helpers/generarJWT.js"
 import User from "../models/User.js"
 
+//192.168.100.7:4000/api/user/login
 const login =async(req,res)=>{
 
-    
-    
+    //obtengo los datos de la pagina login de react
+    const {email,password} = req.body
 
+    console.log('en login')
+    try {
+        //verifico si el email introducido en react existe
+        const usuario = await User.findOne({email})
+
+        //si no existe el error con esta condicional mando el mensaje no existe
+        if(!usuario){
+            return res.status(404).json({msg:'El Usuario no Existe'})
+        }
+
+        //si el password no es el mismo con esta condicional mando el mensaje password incorrecto
+        if(usuario.password !== password){
+            return res.status(403).json({msg:'El password es incorrecto'})
+        }
+
+        //si todo sale bien envio los datos de abajo a react para la autenticacion
+        res.json({
+            nombre:usuario.name,
+            role:usuario.role,
+            token:generarJWT(usuario._id)
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
